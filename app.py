fcc9023b84d1f458cf22f5d3f086ca436f5ecaed
akshubawa <akshubawa70@gmail.com>
2023-11-09 12:44:40 +0530

    fixed cors error

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
 from fastapi import FastAPI, HTTPException
 import csv
+from fastapi.middleware.cors import CORSMiddleware
 from collections import defaultdict, deque
 
 app = FastAPI()
+
+app.add_middleware(
+ CORSMiddleware,
+ allow_origins=["*"],
+ allow_credentials=True,
+ allow_methods=["*"],
+ allow_headers=["*"],
+)
 
 class Graph:
     def __init__(self):
